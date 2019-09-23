4a2757419e9745790a3737a0d00097dbb7b841ad
Alex Predewill <predewill@gmail.com>
2019-09-23 14:42:48 +0300

    upd

--- src/test/java/BaseTest.java
+++ src/test/java/BaseTest.java
@@ -3,23 +3,30 @@ import app.AppConfig;
 import com.codeborne.selenide.Browsers;
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.Selenide;
-import com.codeborne.selenide.WebDriverRunner;
-import io.qameta.allure.Description;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
+import org.testng.asserts.SoftAssert;
+
 import static com.codeborne.selenide.Selenide.*;
 
 public class BaseTest {
 
     protected App app;
 
+    protected SoftAssert softAssert;
+
     @BeforeClass
     public void init() {
+
+        // SET BROWSER CONFIG
         Configuration.browser = Browsers.CHROME;
         Configuration.browserSize = "1920x1080";
         //Configuration.headless = true;
+
+        // CREATE SOFT ASSERT
+        softAssert = new SoftAssert();
     }
 
     @BeforeMethod
@@ -38,7 +45,6 @@ public class BaseTest {
     public void test1() {
         app.loginPage.open();
         app.loginPage.login("atcopybet1@yandex.ru", "test777");
-
     }
 
     @Test
